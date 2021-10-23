fbf3de264f1a1a463be23fee9b4d3bc7c6447034
selfire1 <try2banonym@gmail.com>
2021-10-23 22:22:31 +0200

    ✨ NEW: Add dropdown setting

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
-import { App, Workspace, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Workspace,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from "obsidian";
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
+  mySetting: "default",
+};
 
 export default class MobileDarkModeHelper extends Plugin {
   settings: MyPluginSettings;
@@ -15,104 +22,117 @@ export default class MobileDarkModeHelper extends Plugin {
     await this.loadSettings();
     this.addSettingTab(new SampleSettingTab(this.app, this));
 
+    // Watch for system changes to color theme
 
-    // Watch for system changes to color theme 
+    let media = window.matchMedia("(prefers-color-scheme: dark)");
+    let startTime = "15:10";
+    let endTime = "22:30";
 
-    let media = window.matchMedia('(prefers-color-scheme: dark)');
-    let startTime = '15:10';
-    let endTime = '22:30';
-
-    let currentDate = new Date()
-    let currentHours = currentDate.getHours()
-    let currentMinutes = currentDate.getMinutes()
-
+    let currentDate = new Date();
+    let currentHours = currentDate.getHours();
+    let currentMinutes = currentDate.getMinutes();
 
     let callback = () => {
       if (media.matches) {
-        console.log('Dark mode active');
-        this.updateDarkStyle()
-
+        console.log("Dark mode active");
+        this.updateDarkStyle();
       } else {
-        console.log('Light mode active');
-        this.updateLightStyle()
+        console.log("Light mode active");
+        this.updateLightStyle();
       }
-    }
-    media.addEventListener('change', callback);
+    };
+    media.addEventListener("change", callback);
 
     // Remove listener when we unload
 
-    this.register(() => media.removeEventListener('change', callback));
-    
+    this.register(() => media.removeEventListener("change", callback));
+
     callback();
   }
 
   async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
   onunload() {
-    console.log('System color scheme checking is turned off');
+    console.log("System color scheme checking is turned off");
   }
 
   refreshSystemTheme() {
-    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    const isDarkMode =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    if(isDarkMode){
-        console.log('Dark mode active');
-        this.updateDarkStyle()
-
-      } else {
-        console.log('Light mode active');
-        this.updateLightStyle()
-      }
+    if (isDarkMode) {
+      console.log("Dark mode active");
+      this.updateDarkStyle();
+    } else {
+      console.log("Light mode active");
+      this.updateLightStyle();
+    }
   }
 
   updateDarkStyle() {
     // @ts-ignore
-    this.app.setTheme('obsidian');
+    this.app.setTheme("obsidian");
     // @ts-ignore
-    this.app.vault.setConfig('theme', 'obsidian');
-    this.app.workspace.trigger('css-change');
+    this.app.vault.setConfig("theme", "obsidian");
+    this.app.workspace.trigger("css-change");
   }
 
   updateLightStyle() {
     // @ts-ignore
-    this.app.setTheme('moonstone');
+    this.app.setTheme("moonstone");
     // @ts-ignore
-    this.app.vault.setConfig('theme', 'moonstone');
-    this.app.workspace.trigger('css-change');
+    this.app.vault.setConfig("theme", "moonstone");
+    this.app.workspace.trigger("css-change");
   }
-
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MobileDarkModeHelper;
+  plugin: MobileDarkModeHelper;
 
-	constructor(app: App, plugin: MobileDarkModeHelper) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MobileDarkModeHelper) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+    containerEl.createEl("h2", { text: "Mobile Dark Mode Helper Settings" });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-      }}
+    new Setting(containerEl)
+      .setName("Setting #1")
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your secret")
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+            console.log("Secret: " + value);
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Group By")
+      .addDropdown((dropdown) => 
+        dropdown
+          .addOption("page", "Page")
+          .addOption("tag", "Tag")
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        })
+    );
+  }
+}
